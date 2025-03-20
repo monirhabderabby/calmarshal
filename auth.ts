08a8521741f4aed785d8c921081e2822430506dd
Monir Hossain <monirhrabby.personal@gmail.com>
2025-03-21 04:36:35 +0600

    project setup done

--- auth.ts
+++ auth.ts
@@ -1,5 +1,10 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import { prisma } from "./lib/db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  adapter: PrismaAdapter(prisma),
+  providers: [GitHub, Google],
 });
