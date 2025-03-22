ec0c3cb15ec999826168b90b789566fef11c13cc
Monir Hossain <monirhrabby.personal@gmail.com>
2025-03-23 04:40:01 +0600

    dashboard: layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,14 +1,29 @@
+import { signOut } from "@/auth";
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { requireUser } from "@/lib/hooks";
 import Logo from "@/public/logo.png";
 import { Menu } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import DashboardLinks from "../components/dashboard-links";
+import { ThemeToggle } from "../components/toggle-theme";
 
-export default function DashboardLayout({}: Readonly<{
+export default async function DashboardLayout({
+  children,
+}: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await requireUser();
+
   return (
     <>
       <div className="min-h-screen w-full grid md:grid-col-[220px_1fr] lg:grid-cols-[280px_1fr]">
@@ -43,12 +58,55 @@ export default function DashboardLayout({}: Readonly<{
               </SheetTrigger>
 
               <SheetContent side="left" className="flex flex-col">
-                <nav className="grid gap-2">
+                <nav className="grid gap-2 mt-10">
                   <DashboardLinks />
                 </nav>
               </SheetContent>
             </Sheet>
+
+            <div className="ml-auto flex items-center gap-5">
+              <ThemeToggle />
+
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    variant="secondary"
+                    size="icon"
+                    className="rounded-full"
+                  >
+                    <img
+                      src={session?.user?.image as string}
+                      alt="Profile"
+                      width={20}
+                      height={20}
+                      className="h-full w-full rounded-full"
+                    />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem asChild>
+                    <Link href="/dashboard/settings">Settings</Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <form
+                      action={async () => {
+                        "use server";
+                        await signOut();
+                      }}
+                      className="w-full"
+                    >
+                      <button className="w-full text-left">Log out</button>
+                    </form>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
           </header>
+          <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+            {children}
+          </main>
         </div>
       </div>
     </>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { ThemeProvider } from "./components/theme-provider";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -28,10 +29,17 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
 }
 
-// 1:37:00
+// 2:36:00
